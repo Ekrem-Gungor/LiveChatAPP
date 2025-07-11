6b361ef92dc7a5b7ac2f29777e6bf1739435714d
Ekrem Güngör <ekrmdsgnr@gmail.com>
2025-07-12 02:28:54 +0300

    Add project files.

---
+++ LiveChatAPI/Hubs/ChatHub.cs
@@ -0,0 +1,56 @@
+﻿using LiveChatAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LiveChatAPI.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static readonly List<ChatMessage> _messages = new();
+        static readonly Dictionary<string, string> UserConnections = new();
+        public async Task SendMessage(string user, string message)
+        {
+            ChatMessage msg = new()
+            {
+                User = user,
+                Message = message
+            };
+            _messages.Add(msg);
+
+            await Clients.All.SendAsync("ReceiveMessage", msg);
+        }
+
+        public List<ChatMessage> GetMessages()
+        {
+            return _messages;
+        }
+
+        public async Task Join(string user)
+        {
+            UserConnections[Context.ConnectionId] = user;
+
+            await Clients.All.SendAsync("ReceiveMessage", new ChatMessage
+            {
+                User = "SYSTEM",
+                Message = $"{user} joined the chat"
+            });
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string connectionId = Context.ConnectionId;
+
+            if (UserConnections.TryGetValue(connectionId, out string? user))
+            {
+                await Clients.All.SendAsync("ReciveMessage", new ChatMessage
+                {
+                    User = "SYSTEM",
+                    Message = $"{user} left the chat"
+                });
+
+                UserConnections.Remove(connectionId);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ LiveChatAPI/Program.cs
@@ -0,0 +1,41 @@
+using LiveChatAPI.Hubs;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:5173"); // React Vite portu
+    });
+});
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowReactApp");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
