96da6876162be0caf9432ddd8e131ddbd5f69021
Ekrem Güngör <ekrmdsgnr@gmail.com>
2025-07-17 15:43:59 +0300

    - feat:   - Identity kullanılmak üzere `AspNetCore.Identity.EntityFrameworkCore` eklendi   - Veri tabanını temsil edecek kullanıcı sınıfları oluşturuldu.   - Sınıflar için enums eklendi.

---
+++ src/core/DevBudy.DOMAIN/Entities/Concretes/AppUserProfile.cs
@@ -0,0 +1,25 @@
+﻿using DevBudy.DOMAIN.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBudy.DOMAIN.Entities.Concretes
+{
+    public class AppUserProfile : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+        public string? Bio { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        //public Gender Gender { get; set; }
+
+        // Relational Properties
+
+        public virtual AppUser AppUser { get; set; }
+    }
+}
